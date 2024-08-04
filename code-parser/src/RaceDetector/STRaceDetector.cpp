0e8770c49c68a7cb8aad1af3533a75f55294aacb
Tao Bao <tao@sec3.dev>
2024-08-04 16:44:23 -0500

    chore(parser): traverse dirs via std::filesystem

--- code-parser/src/RaceDetector/STRaceDetector.cpp
+++ code-parser/src/RaceDetector/STRaceDetector.cpp
@@ -1,46 +1,27 @@
-#include <dirent.h>
-
 #include <any>
+#include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <memory>
 #include <set>
 #include <string>
 #include <vector>
 
 #include <llvm/ADT/StringRef.h>
-#include <llvm/Analysis/AssumptionCache.h>
-#include <llvm/Analysis/TypeBasedAliasAnalysis.h>
-#include <llvm/IR/IRPrintingPasses.h>
 #include <llvm/IR/LLVMContext.h>  // for llvm LLVMContext
 #include <llvm/IR/LegacyPassManager.h>
-#include <llvm/IR/Mangler.h>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/Verifier.h>
 #include <llvm/IRReader/IRReader.h>  // IR reader for bit file
 #include <llvm/Support/CommandLine.h>
-#include <llvm/Support/ErrorOr.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/InitLLVM.h>
-#include <llvm/Support/MemoryBuffer.h>
-#include <llvm/Support/Path.h>
-#include <llvm/Support/Regex.h>
-#include <llvm/Support/Signals.h>    // signal for command line
 #include <llvm/Support/SourceMgr.h>  // for SMDiagnostic
 #include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/raw_ostream.h>
-#include <llvm/Transforms/IPO/AlwaysInliner.h>
 #include <mlir/ExecutionEngine/ExecutionEngine.h>
-#include <mlir/ExecutionEngine/OptUtils.h>
 #include <mlir/IR/AsmState.h>
-#include <mlir/IR/MLIRContext.h>
-#include <mlir/IR/Verifier.h>
-#include <mlir/InitAllDialects.h>
-#include <mlir/Parser.h>
 #include <mlir/Pass/Pass.h>
 #include <mlir/Pass/PassManager.h>
 #include <mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h>
 #include <mlir/Target/LLVMIR/Export.h>
-#include <mlir/Transforms/Passes.h>
 
 #include <conflib/conflib.h>
 #include <o2/Util/Log.h>
@@ -78,7 +59,7 @@ cl::opt<int> NUM_UP_BOUND(
     "ub", cl::desc("set upper bound of the number of functions"),
     cl::value_desc("number"), cl::init(1000000));
 
-logger::LoggingConfig initLoggingConf() {
+static logger::LoggingConfig initLoggingConf() {
   logger::LoggingConfig config;
 
   config.enableProgress = conflib::Get<bool>("XenableProgress", true);
@@ -109,197 +90,90 @@ logger::LoggingConfig initLoggingConf() {
   return config;
 }
 
-static std::unique_ptr<Module> loadFile(const std::string &FN,
-                                        LLVMContext &Context,
-                                        bool abortOnFail) {
-  SMDiagnostic Err;
-  std::unique_ptr<Module> Result;
-  Result = parseIRFile(FN, Err, Context);
+static void process(const std::string &filename, const std::string &method,
+                    size_t line, std::string contents) {
+  std::replace(contents.begin(), contents.end(), '\t', ' ');
 
-  if (!Result) {
-    if (abortOnFail) {
-      Err.print("racedetect", llvm::errs());
-      abort();
-    }
-
-    LOG_ERROR("error loading file: {}", FN);
-    return nullptr;
-  }
-  return Result;
-}
-
-std::set<const Function *> unExploredFunctions;
-const std::set<llvm::StringRef> SKIPPED_APIS{
-    "llvm.", "pthread_", "__kmpc_",  ".omp.", "omp_",   "_ZNSt",
-    "_ZSt",  "_ZNKSt",   "_ZN14o2_", "__o2_", "printf", "je_"};
-void addExploredFunction(const Function *f) { unExploredFunctions.erase(f); }
-void initUnexploredFunctions(Module *module) {
-  auto &functionList = module->getFunctionList();
-  for (auto &function : functionList) {
-    auto func = &function;
-    if (!func->isDeclaration()) {
-      bool insert = true;
-      // do not count llvm.* pthread_* __kmpc_* __smalltalk_ .omp. omp_ std::
-      for (auto str : SKIPPED_APIS) {
-        if (func->getName().startswith(str)) {
-          insert = false;
-          break;
-        }
-      }
-      if (insert) unExploredFunctions.insert(func);
-    }
-  }
-}
-
-void computeUnexploredFunctions() {
-  if (unExploredFunctions.size() > 0) {
-    llvm::outs() << "\n============= unexplored function ==========\n";
-    for (auto func : unExploredFunctions)
-      llvm::outs() << (func->getName()) << "\n";
-    // TODO: print potential entry points
-    // no caller
-  }
-}
-
-void initRaceDetect() {
-  LOG_INFO("Loading IR From File: {}", TargetModulePath);
-  auto module = loadFile(TargetModulePath, *(new LLVMContext()), true);
-  // ThreadProfileRewriter::rewriteModule(module.get());
-
-  // Initialize passes
-  // PassRegistry &Registry = *PassRegistry::getPassRegistry();
-
-  // initializeCore(Registry);
-  // initializeScalarOpts(Registry);
-  // initializeIPO(Registry);
-  // initializeAnalysis(Registry);
-  // initializeTransformUtils(Registry);
-  // initializeInstCombine(Registry);
-  // initializeAggressiveInstCombine(Registry);
-  // initializeInstrumentation(Registry);
-  // initializeTarget(Registry);
-
-  llvm::legacy::PassManager analysisPasses;
-  // analysisPasses.add(new RaceDetectionPass());
-  initUnexploredFunctions(module.get());
-  analysisPasses.run(*module);
-  computeUnexploredFunctions();
-}
-// using namespace antlrcpp;
-
-st::ModuleAST moduleAST;
-
-void process(std::string filename, std::string methodname, size_t line,
-             std::string s2) {
-  std::replace(s2.begin(), s2.end(), '\t', ' ');
-  auto *input = new ANTLRInputStream(s2);
-
+  // Process file contents via ANTLR lexer.
+  auto *input = new ANTLRInputStream(contents);
   auto *lexer = new RustLexer(input);
   auto *tokens = new CommonTokenStream(lexer);
-
   tokens->fill();
 
+  // Parse the tokens.
   auto *parser = new RustParser(tokens);
-
-  // auto *tree = parser->module();
-  // llvm::outs() << tree->toStringTree(parser) << "\n";
-  // llvm::outs() << "-----------"
-  //              << "\n";
-  // // TODO: add listener
-  // auto *listener = new STParserListener(filename, methodname, line);
-  // antlr4::tree::ParseTreeWalker::DEFAULT.walk(listener, tree);
-  // auto data = listener->getData();
-  // moduleAST.addModule(std::move(data));
-
-  STParserVisitor visitor(filename, methodname, line);
-  // std::vector<stx::FunctionAST *> res = visitor.visitCrate(parser->crate());
-
-  std::any any_res = visitor.visitCrate(parser->crate()); 
-  std::vector<stx::FunctionAST*> res = std::any_cast<std::vector<stx::FunctionAST*>>(any_res);
-
+  STParserVisitor visitor(filename, method, line);
+  auto any_res = visitor.visitCrate(parser->crate());
+  std::vector<stx::FunctionAST *> res(
+      std::any_cast<std::vector<stx::FunctionAST *>>(std::move(any_res)));
   processResults.insert(processResults.end(), res.begin(), res.end());
 }
 
-// c++17 only
-// using std::filesystem::directory_iterator;
-void initParserForFile(stx::ModuleAST *module, std::string &fullPathName) {
-  // for (const auto & entry : directory_iterator(path)){
-  //     StringRef name(entry.path());
-  llvm::outs() << "  source file: " << fullPathName << "\n";
+static void initParserForFile(const std::string &full_path) {
+  llvm::outs() << "  source file: " << full_path << "\n";
 
-  StringRef name(fullPathName);
-  std::ifstream ws(name.str());
+  std::ifstream ws(full_path);
   std::string contents((std::istreambuf_iterator<char>(ws)),
                        std::istreambuf_iterator<char>());
 
-  if (DEBUG_SOL) {
-    if (name.endswith(".toml")) {
-      llvm::outs() << "=== Test WS Start ===\n";
-      llvm::outs() << contents << "\n";
-      llvm::outs() << "=== Test WS End ===\n";
-    } else if (name.endswith(".rs")) {
-      llvm::outs() << "=== Rust Source Start ===\n";
-      llvm::outs() << contents << "\n";
-      llvm::outs() << "=== Rust Source End ===\n";
-    }
-  }
-  auto found1 = fullPathName.find_last_of("/");
-  auto found2 = fullPathName.find_last_of(".");
-  auto fnBaseName = fullPathName.substr(found1 + 1, found2 - found1 - 1);
-  process(fullPathName, fnBaseName, 0, contents);
+  auto path = std::filesystem::path(full_path);
+  auto base_name = path.stem().string();
+  process(full_path, base_name, 0, contents);
 }
 
-bool handleRustFile(stx::ModuleAST *module, std::string &fullPathName) {
-  // is it a file?
-  FILE *fptr = fopen(fullPathName.c_str(), "r");
-  if (fptr == NULL) {
-    if (DEBUG_SOL)
-      llvm::errs() << "input file does not exist: " << fullPathName << "\n";
+static bool handleRustFile(const std::string &full_path) {
+  std::ifstream fptr(full_path);
+  if (!fptr) {
+    if (DEBUG_SOL) {
+      llvm::errs() << "input file does not exist: " << full_path << "\n";
+    }
     return false;
   }
-  // File exists hence close file
-  fclose(fptr);
-  initParserForFile(module, fullPathName);
-
+  initParserForFile(full_path);
   return true;
 }
 
-bool handleDiretory(DIR *dir, stx::ModuleAST *module, std::string &fullPathName) {
-  StringRef pathname_stringref(fullPathName);
-  if (pathname_stringref.endswith("/src")) {
-    // ends with /src
-    llvm::outs() << "source path: " << fullPathName << "\n";
-  } else if (fullPathName == module->path)
-    llvm::outs() << "path: " << fullPathName << "\n";
-  else {
-    llvm::outs() << "subpath: " << fullPathName << "\n";
-    // if subpath contains Cargo.toml, skip
-    struct dirent *entry;
-    DIR *dir2 = opendir(fullPathName.c_str());
-    while ((entry = readdir(dir2)) != NULL) {
-      StringRef dname(entry->d_name);
-      // llvm::outs() << "dname: " << dname << "\n";
-      if (dname.equals("Xargo.toml")) return true;
+static bool handleDiretory(stx::ModuleAST *mod, const std::filesystem::path &dir_path) {
+  std::string dir_path_str(dir_path.string());
+  llvm::StringRef pathname(dir_path_str);
+  if (pathname.endswith("/src")) {
+    llvm::outs() << "source path: " << dir_path << "\n";
+  } else if (pathname == mod->path) {
+    llvm::outs() << "path: " << dir_path << "\n";
+  } else {
+    llvm::outs() << "subpath: " << dir_path << "\n";
+    // if subpath contains Xargo.toml, skip
+    for (const auto &entry : std::filesystem::directory_iterator(dir_path)) {
+      if (entry.is_regular_file() && entry.path().filename() == "Xargo.toml") {
+        return true;
+      }
     }
-    closedir(dir2);
   }
-  struct dirent *entry;
-  while ((entry = readdir(dir)) != NULL) {
-    StringRef dname(entry->d_name);
-    if (DEBUG_SOL) llvm::outs() << "dname: " << dname << "\n";
-    // std::transform(dname.begin(), dname.end(), dname.begin(), ::tolower);
-    // llvm::outs() << "fullPathName: " << fullPathName << "\n";
-    std::string fullPathName2 =
-        fullPathName + "/" + entry->d_name;  // realpath(dname.data(), NULL);
-    // llvm::outs() << "fullPathName2: " << fullPathName2 << "\n";
 
-    DIR *dir2 = opendir(fullPathName2.c_str());
-    if (dir2 == NULL) {
-      if (dname.endswith(".rs"))
-        handleRustFile(module, fullPathName2);
-      else if (dname.endswith("argo.toml")) {
-        module->path_config = fullPathName2;
-        auto data = toml::parse(fullPathName2);
+  for (const auto &entry : std::filesystem::directory_iterator(dir_path)) {
+    std::string entry_str(entry.path().string());
+    llvm::StringRef dname(entry_str);
+    if (DEBUG_SOL) llvm::outs() << "dname: " << dname << "\n";
+
+    if (entry.is_directory()) {
+      if (!dname.equals("tests") &&
+          !dname.equals("js") && !dname.equals("cli") &&
+          !dname.equals("logs") && !dname.equals("target") &&
+          !dname.equals("debug") && !dname.equals("migrations") &&
+          !dname.equals("services") &&
+          !dname.equals("proptest-regressions")) {
+        // nested diretory
+        handleDiretory(mod, entry.path());
+      }
+      continue;
+    }
+
+    if (entry.is_regular_file()) {
+      if (dname.endswith(".rs")) {
+        handleRustFile(entry_str);
+      } else if (dname.endswith("argo.toml")) {
+        mod->path_config = entry_str;
+        auto data = toml::parse(entry_str);
         if (data.contains("profile") &&
             data.at("profile").count("release") != 0) {
           const toml::value answer1 =
@@ -307,20 +181,13 @@ bool handleDiretory(DIR *dir, stx::ModuleAST *module, std::string &fullPathName)
           const toml::value answer2 =
               toml::get<toml::table>(answer1).at("release");
           if (answer2.count("overflow-checks")) {
-            // auto line = answer2.location().line();
-            // auto column = answer2.location().column();
             const auto hasOverFlowCheck =
                 toml::find<bool>(data, "profile", "release", "overflow-checks");
             llvm::outs() << dname << ": hasOverFlowCheck: " << hasOverFlowCheck
                          << "\n";
-            //<< " line: " << line << " column: " << column << "\n";
-            if (hasOverFlowCheck)
-              module->configMap["profile.release.overflow-checks"] = "1";
-
-            // const toml::value overFlowCheck =
-            //     toml::get<toml::table>(answer2).at("overflow-checks");
-            // auto hasOverFlowCheck =
-            //     toml::get_or(overFlowCheck, false);  // this works
+            if (hasOverFlowCheck) {
+              mod->configMap["profile.release.overflow-checks"] = "1";
+            }
           }
         }
 
@@ -349,7 +216,7 @@ bool handleDiretory(DIR *dir, stx::ModuleAST *module, std::string &fullPathName)
             // llvm::outs() << dname << ": spl-token version: " << version
             //              << " default-features: " << features << "\n";
             if (!version.empty()) {
-              module->configMap["dependencies.spl-token.version"] = version;
+              mod->configMap["dependencies.spl-token.version"] = version;
             }
           }
 
@@ -365,7 +232,7 @@ bool handleDiretory(DIR *dir, stx::ModuleAST *module, std::string &fullPathName)
               }
             }
             if (!version.empty()) {
-              module->configMap["dependencies.anchor-lang.version"] = version;
+              mod->configMap["dependencies.anchor-lang.version"] = version;
             }
           }
 
@@ -381,7 +248,7 @@ bool handleDiretory(DIR *dir, stx::ModuleAST *module, std::string &fullPathName)
               }
             }
             if (!version.empty()) {
-              module->configMap["dependencies.anchor-spl.version"] = version;
+              mod->configMap["dependencies.anchor-spl.version"] = version;
             }
           }
           if (data.at("dependencies").count("solana-program") != 0) {
@@ -396,45 +263,37 @@ bool handleDiretory(DIR *dir, stx::ModuleAST *module, std::string &fullPathName)
               }
             }
             if (!version.empty()) {
-              module->configMap["dependencies.solana_program.version"] = version;
+              mod->configMap["dependencies.solana_program.version"] = version;
             }
           }
         }
       }
-    } else if (!dname.startswith(".") && !dname.equals("tests") &&
-               !dname.equals("js") && !dname.equals("cli") &&
-               !dname.equals("logs") && !dname.equals("target") &&
-               !dname.equals("debug") && !dname.equals("migrations") &&
-               !dname.equals("services") &&
-               !dname.equals("proptest-regressions")) {
-      // nested diretory
-      handleDiretory(dir2, module, fullPathName2);
-      closedir(dir2);
     }
   }
   return true;
 }
 
-bool initParser(stx::ModuleAST *module) {
+static bool initParser(stx::ModuleAST *mod) {
   LOWER_BOUND_ID = NUM_LOW_BOUND;
-  StringRef path(TargetModulePath);
-  std::string fullPathName = realpath(path.data(), NULL);
-  module->path = fullPathName;
 
-  DIR *dir = opendir(path.data());
-  if (dir == NULL) {
-    if (path.endswith(".rs")) handleRustFile(module, fullPathName);
-  } else {
-    handleDiretory(dir, module, fullPathName);
-    closedir(dir);
+  llvm::StringRef path(TargetModulePath);
+  std::string full_path = std::filesystem::canonical(path.str()).string();
+  mod->path = full_path;
+
+  if (std::filesystem::is_regular_file(full_path)) {
+    if (path.endswith(".rs")) {
+      handleRustFile(full_path);
+    }
+  } else if (std::filesystem::is_directory(full_path)) {
+    handleDiretory(mod, full_path);
   }
   for (auto functionAst : processResults) {
-    module->addFunctionAST(functionAst);
+    mod->addFunctionAST(functionAst);
   }
   return true;
 }
 
-int dumpLLVMIR(mlir::ModuleOp module) {
+static int dumpLLVMIR(mlir::ModuleOp module) {
   // Register the translation to LLVM IR with the MLIR context.
   mlir::registerLLVMDialectTranslation(*module->getContext());
 
@@ -450,14 +309,6 @@ int dumpLLVMIR(mlir::ModuleOp module) {
   llvm::InitializeNativeTargetAsmPrinter();
   mlir::ExecutionEngine::setupTargetTriple(llvmModule.get());
 
-  /// Optionally run an optimization pipeline over the llvm module.
-  //   auto optPipeline = mlir::makeOptimizingTransformer(
-  //       /*optLevel=*/ 0, /*sizeLevel=*/0,
-  //       /*targetMachine=*/nullptr);
-  //   if (auto err = optPipeline(llvmModule.get())) {
-  //     llvm::errs() << "Failed to optimize LLVM IR " << err << "\n";
-  //     return -1;
-  //   }
   if (ConfigDumpIR || ConfigOutputFile != "t.ll") {
     std::error_code err;
     llvm::raw_fd_ostream outfile(ConfigOutputFile, err, llvm::sys::fs::OF_None);
@@ -475,13 +326,11 @@ int dumpLLVMIR(mlir::ModuleOp module) {
   return 0;
 }
 
-void initLLVMIR(stx::ModuleAST *moduleAST) {
+static void initLLVMIR(stx::ModuleAST *moduleAST) {
   // Register any command line options.
   mlir::registerAsmPrinterCLOptions();
   mlir::registerMLIRContextCLOptions();
   mlir::registerPassManagerCLOptions();
-
-  // If we aren't dumping the AST, then we are compiling with/to MLIR.
 
   mlir::MLIRContext context;
   mlir::OwningOpRef<mlir::ModuleOp> module;
@@ -493,31 +342,15 @@ void initLLVMIR(stx::ModuleAST *moduleAST) {
 
   module = stx::mlirGenFull(context, *moduleAST);
 
-  // if (auto temp =
-  // module->lookupSymbol<mlir::LLVM::LLVMFuncOp>("anonymousFun1")){
-  //                 llvm::outs() << "try to remove redefinition of symbol
-  //                 anonymousFun1\n";
-  // }
+  mlir::PassManager pm(&context);
+  // Apply any generic pass manager command line options and run the
+  // pipeline.
+  applyPassManagerCLOptions(pm);
 
-  // if (false)
-  {
-    mlir::PassManager pm(&context);
-    // Apply any generic pass manager command line options and run the
-    // pipeline.
-    applyPassManagerCLOptions(pm);
-
-    // Partially lower the toy dialect with a few cleanups afterwards.
-    // pm.addPass(mlir::st::createLowerToAffinePass());
-
-    // mlir::OpPassManager &optPM = pm.nest<mlir::FuncOp>();
-    // optPM.addPass(mlir::createCanonicalizerPass());
-    // optPM.addPass(mlir::createCSEPass());
-
-    // Finish lowering the toy IR to the LLVM dialect.
-    pm.addPass(mlir::st::createLowerToLLVMPass());
-    if (mlir::failed(pm.run(*module))) {
-      if (DEBUG_SOL) llvm::errs() << "Errors in createLowerToLLVMPass.\n";
-    }
+  // Finish lowering the IR to the LLVM dialect.
+  pm.addPass(mlir::st::createLowerToLLVMPass());
+  if (mlir::failed(pm.run(*module))) {
+    if (DEBUG_SOL) llvm::errs() << "Errors in createLowerToLLVMPass.\n";
   }
 
   dumpLLVMIR(*module);
@@ -536,9 +369,8 @@ int main(int argc, char **argv) {
   auto logConfig = initLoggingConf();
   logger::init(logConfig);
 
-  stx::ModuleAST *module = new stx::ModuleAST();
-  bool success = initParser(module);
-  if (success) initLLVMIR(module);
-  // initRaceDetect();
+  stx::ModuleAST *mod = new stx::ModuleAST();
+  bool success = initParser(mod);
+  if (success) initLLVMIR(mod);
   return 0;
 }
